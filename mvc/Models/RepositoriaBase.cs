0761e857a72356b84c90f8ab54c084d88ca81afd
Lucas <serranolucas1000@gmail.com>
2026-08-20 13:53:28 -0300

    Creacion repoBase

---
+++ mvc/Models/RepositoriaBase.cs
@@ -0,0 +1,17 @@
+
+
+namespace Inmobiliaria_.Net_Core.Models
+{
+    public abstract class RepositorioBase
+    {
+        protected readonly IConfiguration configuration;
+        protected readonly string connectionString;
+
+        protected RepositorioBase(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            connectionString = configuration["ConnectionStrings:DefaultConnection"];
+
+        }
+    }
+}
--- mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
+++ mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa5913a047b82c16193ce59dd332041d873dd9e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8edc81f00fececb0bd955b053a4f3e59f6d61cf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
